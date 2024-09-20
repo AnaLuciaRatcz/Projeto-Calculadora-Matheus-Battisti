1e0296c2df93858dea8b78362cb4e85c3882c591
Ana Lucia Ratuczne <alratuczne@yahoo.com.br>
2024-09-20 16:59:26 -0300

    atualizações

--- js/script.js
+++ js/script.js
@@ -10,8 +10,11 @@ class Calcutator{
     }
 
     // add digit to calculator screen
+
     addDigit(digit){
+
         // check if current operation already has a dot
+
         if(digit === "." && this.currentOperationText.innerText.includes(".")){
             return;
         }
@@ -21,11 +24,15 @@ class Calcutator{
     }
 
     // process all calculator operations
+
     processOperation(operation){
 
         //check if current is empty
+
         if(this.currentOperationText.innerText === "" && operation !== "C"){
+
             // change operation
+
             if(this.previousOperationText.innerText !== ""){
                 this.changeOperation(operation);
             }
@@ -34,6 +41,7 @@ class Calcutator{
         }
         
         // get current and previous value
+
         let operationValue;
         const previous = +this.previousOperationText.innerText.split(" ")[0];
         const current = +this.currentOperationText.innerText;
@@ -73,6 +81,7 @@ class Calcutator{
     }
 
     // change values of the calculator screen
+
     updateScreen(
         operationValue = null, 
         operation = null, 
@@ -85,18 +94,22 @@ class Calcutator{
 
        this.currentOperationText.innerText += this.currentOperation;
        } else{
+
         // check if value is zero, if it is, just add current value
+
         if(previous === 0){
             operationValue = current
         }
 
         // add current value to previous
+
         this.previousOperationText.innerText = `${operationValue} ${operation}`
         this.currentOperationText.innerText = "";
 
        }
     }
     // change math operation
+
     changeOperation(operation){
         const mathOperations= ["*", "/", "+", "-"]
         if(!mathOperations.includes(operation)){
@@ -105,27 +118,34 @@ class Calcutator{
         this.previousOperationText.innerText = 
         this.previousOperationText.innerText.slice(0,-1) + operation;
     }
+
     // delete the last digit
+
     processDelOperator(){
         this.currentOperationText.innerText = 
         this.currentOperationText.innerText.slice(0, -1)
     }
+
     // clear current operation
+
     processClearCurrentOperation(){
         this.currentOperationText.innerText ="";
     }
+
     // clear all operations
+
     processClearOperation(){
         this.currentOperationText.innerText ="";
         this.previousOperationText.innerText ="";
     }
+
     // process an operation (=)
+    
     processEqualOperator(){
         const operation = previousOperationText.innerText.split(" ")[1];
 
         this.processOperation(operation);
     }
-
 }
 
 const calc = new Calcutator(previousOperationText, currentOperationText);
